bd6aba26945a3d4ae5845894f73c76b16e1f5d59
David De Vargas <david.devargas@zemoga.com>
2022-03-11 07:48:57 -0500

    feat (getStaticProps): on index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,27 @@
-import { Button } from '@nextui-org/react'
-import type { NextPage } from 'next'
+import { NextPage, GetStaticProps } from 'next'
 
-const HomePage: NextPage = () => {
+import { Button } from '@nextui-org/react'
+
+import { Layout } from '../components/layouts'
+
+const HomePage: NextPage = (props) => {
+  console.log(props)
   return (
-    <>
-    <Button color="gradient">hola</Button>
-    </>
+    <Layout title='Listado de Pókemons'>
+      <Button color="gradient">hola</Button>
+    </Layout>
   )
+}
+
+
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  console.log('hola mundo')
+  return {
+    props: {
+      name: 'David'
+    }
+  }
 }
 
 export default HomePage
