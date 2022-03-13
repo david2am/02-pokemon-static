c4252c59d37fe88b017e62b623b834e6a2cfdd6f
David De Vargas <david.devargas@zemoga.com>
2022-03-13 16:51:04 -0500

    feat (pokemon/[id]): design

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,13 +1,67 @@
 import { GetStaticProps, NextPage, GetStaticPaths } from 'next';
 
+import { Grid, Card, Text, Button, Container, Image } from '@nextui-org/react';
+
 import { pokeApi } from '../../api';
 import { Layout } from '../../components/layouts';
 import { Pokemon } from '../../interfaces';
 
-export const PokemonPage: NextPage<Pokemon> = ({ name }) => {
+export const PokemonPage: NextPage<Pokemon> = ({ name, sprites }) => {
   return (
     <Layout title={`Pokemon ${name}`}>
-      <h1>{name}</h1>
+      <Grid.Container css={{ marginTop: '5px' }} gap={2}>
+        <Grid xs={12} sm={4}>
+          <Card hoverable css={{ padding: '30px' }}>
+            <Card.Body>
+              <Card.Image
+                src={sprites.other?.dream_world.front_default || '/no-image.png'}
+                alt={name}
+                width="100%"
+                height={200}
+              />
+            </Card.Body>
+          </Card>
+        </Grid>
+
+        <Grid xs={12} sm={8}>
+          <Card>
+            <Card.Header css={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Text h1 transform='capitalize'>{name}</Text>
+              <Button color='gradient' ghost>Guardar en favoritos</Button>
+            </Card.Header>
+
+            <Card.Body>
+              <Text size={30}>Sprites</Text>
+              <Container display='flex' direction='row'>
+                <Image
+                  src={sprites.front_default}
+                  alt={name}
+                  width={100}
+                  height={100}
+                />
+                <Image
+                  src={sprites.back_default}
+                  alt={name}
+                  width={100}
+                  height={100}
+                />
+                <Image
+                  src={sprites.front_shiny}
+                  alt={name}
+                  width={100}
+                  height={100}
+                />
+                <Image
+                  src={sprites.back_shiny}
+                  alt={name}
+                  width={100}
+                  height={100}
+                />
+              </Container>
+            </Card.Body>
+          </Card>
+        </Grid>
+      </Grid.Container>
     </Layout>
   )
 }
