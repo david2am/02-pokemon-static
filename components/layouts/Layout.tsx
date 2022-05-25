fc42ae019eb46aacf6c365ca96491604388a1e19
David De Vargas <david.devargas@zemoga.com>
2022-05-25 13:33:31 -0500

    feat(Layout): adding metatags

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -8,14 +8,20 @@ interface Props {
     title?: string;
 }
 
+const origin = (typeof window === 'undefined') ? '' : window.location.origin
+
 export const Layout: FC<Props> = ({ children, title }) => {
   return (
     <>
         <Head>
-            <title>{title || 'Pokemon App'}</title>
+            <title>{ title || 'Pokemon App' }</title>
             <meta name="author" content="David De Vargas" />
-            <meta name="description" content={`Pokemon desc ${title}`} />
-            <meta name="keywords" content={`${title}, Pokemon, pokedex`} />
+            <meta name="description" content={`Pokemon desc ${ title }`} />
+            <meta name="keywords" content={`${ title }, Pokemon, pokedex`} />
+
+            <meta property="og:title" content={`Información sobre: ${ title }`} />
+            <meta property="og:description" content={`Esta es la página sobre ${ title }`} />
+            <meta property="og:image" content={`${ origin }/img/banner.png`} />
         </Head>
 
         <Navbar />
